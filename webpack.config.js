fee390da9812adbf44bfa3eca3d9b39f3dc6f075
Dave Ross <dave@davidmichaelross.com>
2017-03-12 18:03:33 -0400

    webpack-node-externals

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var path = require('path');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
 	entry: "./app/src/server.ts",
@@ -8,12 +9,19 @@ module.exports = {
 	module: {
 		loaders: [
 			// note that babel-loader is configured to run after ts-loader
-			{ test: /\.ts(x?)$/, loader: "babel-loader?presets[]=es2015!ts-loader" }
+			{
+				test: /\.ts(x?)$/,
+				loader: "babel-loader?presets[]=es2015!ts-loader"
+			},
+			{
+				test: /\.js$/,
+				loader: "babel-loader"
+			}
 		]
 	},
 	resolve: {
 		extensions: [".webpack.js", ".web.js", ".ts", ".js"]
 	},
 	target: "node",
-	externals: [ 'sqlite3' ]
+	externals: [nodeExternals()]
 }

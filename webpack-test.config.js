2f875512afc3d9a97d051733cfc706b431b4bc45
Dave Ross <dave@davidmichaelross.com>
2017-03-21 09:11:37 -0400

    Mocha + Chai unit tests working

---
+++ webpack-test.config.js
@@ -0,0 +1,32 @@
+var path = require('path');
+var webpack = require("webpack");
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+	entry: "./test/src/test.ts",
+	output: {
+		filename: "test/tests.js"
+	},
+	module: {
+		loaders: [
+			// note that babel-loader is configured to run after ts-loader
+			{
+				test: /\.ts(x?)$/,
+				loader: "babel-loader?presets[]=es2015!ts-loader"
+			},
+			{
+				test: /\.js$/,
+				loader: "babel-loader"
+			}
+		]
+	},
+	plugins: [
+		new webpack.optimize.UglifyJsPlugin({minimize: true})
+
+	],
+	resolve: {
+		extensions: [".webpack.js", ".web.js", ".ts", ".js"]
+	},
+	target: "node",
+	externals: [nodeExternals()]
+}

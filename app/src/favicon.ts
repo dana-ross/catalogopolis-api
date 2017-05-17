22dba1d1af5501cffe2a5d1d342e3c4413f20c46
Dave Ross <dave@davidmichaelross.com>
2017-05-16 21:44:12 -0400

    Favicon handler

---
+++ app/src/favicon.ts
@@ -0,0 +1,24 @@
+/**
+ * @file Favicon.ico handler middleware
+ * @author Dave Ross <dave@davidmichaelross.com>
+ */
+
+import { Application, Request, Response } from 'express'
+
+/**
+ * Register middleware to silently "handle" favicon requests
+ *
+ * @param server {Application} Express application
+ */
+export default function (server: Application) {
+	server.use((req: Request, res: Response, next: Function) => {
+		if (req.url === '/favicon.ico') {
+			res.status(200)
+			res.type('image/x-icon')
+			res.send('')
+			return
+		}
+
+		next()
+	})
+}
--- app/src/server.ts
+++ app/src/server.ts
@@ -1,3 +1,4 @@
+import { Request } from 'express';
 /**
  * @file Catalogopolis API Bootstrap/Initialization
  * @author Dave Ross <dave@davidmichaelross.com>
@@ -11,6 +12,7 @@ import { Database } from "sqlite3"
 import graphql from "./graphql"
 import * as path from "path"
 import analytics from "./analytics"
+import favicon from "./favicon"
 
 var connection: Database = new Database('catalogopolis-api.sqlite');
 
@@ -20,6 +22,7 @@ if(!process.env.CATALOGOPOLIS_API_UA) {
 	throw('CATALOGOPOLIS_API_UA environment variable must be set')
 }
 
+favicon(server)
 analytics(server, process.env.CATALOGOPOLIS_API_UA)
 restv1(server, connection);
 graphql(server, connection);

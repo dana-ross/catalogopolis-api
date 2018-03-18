fc3238a5051e093671232b2ea01a348e654d49e4
Dave Ross <dave@davidmichaelross.com>
2018-03-18 14:50:44 -0400

    Apply CORS to all API URLs

--- app/src/cors.ts
+++ app/src/cors.ts
@@ -7,7 +7,7 @@ import { Application, Request, Response } from "polka"
 
 export default function corsAllowAll(server: Application) {
 
-	server.use("/graphql", function (req: Request, res: Response, next: Function) {
+	server.use("/*", function (req: Request, res: Response, next: Function) {
 		res.setHeader('Access-Control-Allow-Origin', '*')
 		res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS' )
 		res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')

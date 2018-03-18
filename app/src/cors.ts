15defc423d8ead494e4e1e8292a298a1a6fa7dcd
Dave Ross <dave@davidmichaelross.com>
2018-03-18 14:18:47 -0400

    CORS OPTIONS support

--- app/src/cors.ts
+++ app/src/cors.ts
@@ -10,4 +10,11 @@ export default function corsAllowAll(server: Application) {
 		res.setHeader('Access-Control-Allow-Origin', '*')
 		next()
 	})
+
+	server.options("/*", function(req, res, next){
+		res.header('Access-Control-Allow-Origin', '*')
+		res.header('Access-Control-Allow-Methods', 'GET,OPTIONS')
+		next()
+	})
+
 }

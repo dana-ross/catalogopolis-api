45078ec6a5eff1c964c6e2d396ed5d6e1490213c
Dave Ross <dave@davidmichaelross.com>
2018-03-18 14:37:55 -0400

    CORS OPTIONS support

--- app/src/cors.ts
+++ app/src/cors.ts
@@ -8,7 +8,7 @@ import { Application, Request, Response } from "polka"
 export default function corsAllowAll(server: Application) {
 
 	server.use("/graphql", function (req: Request, res: Response, next: Function) {
-		res.set('Access-Control-Allow-Origin', '*');
+		res.setHeader('Access-Control-Allow-Origin', '*');
 		if (req.method === 'OPTIONS') {
 		  res.sendStatus(200);
 		} else {

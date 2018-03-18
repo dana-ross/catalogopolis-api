d9c1dd71518b5ce57bbdb665c08c3c6588396105
Dave Ross <dave@davidmichaelross.com>
2018-03-18 15:20:22 -0400

    CORS applies everywhere

--- app/src/cors.ts
+++ app/src/cors.ts
@@ -7,20 +7,16 @@ import { Application, Request, Response } from "polka"
 
 export default function corsAllowAll(server: Application) {
 
-	server.use("*", function (req: Request, res: Response, next: Function) {
-
-		res.setHeader({
-			'Access-Control-Allow-Origin': '*',
-			'Access-Control-Allow-Methods': 'GET, OPTIONS',
-			'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
-		})
-
+	server.use(function (req: Request, res: Response, next: Function) {
+		res.setHeader('Access-Control-Allow-Origin', '*')
+		res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS' )
+		res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
 		if (req.method === 'OPTIONS') {
 		  res.statusCode = 200
 		  res.end()
 		} else {
 		  next()
 		}
-	});
+	})
 
 }

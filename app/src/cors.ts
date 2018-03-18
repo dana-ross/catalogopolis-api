69e3fad00123ae2352c6534e65e7ebea0095d565
Dave Ross <dave@davidmichaelross.com>
2018-03-18 14:31:07 -0400

    CORS OPTIONS support

--- app/src/cors.ts
+++ app/src/cors.ts
@@ -9,7 +9,6 @@ export default function corsAllowAll(server: Application) {
 
 	server.use("/graphql", function (req: Request, res: Response, next: Function) {
 		res.header('Access-Control-Allow-Origin', '*');
-		res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 		if (req.method === 'OPTIONS') {
 		  res.sendStatus(200);
 		} else {

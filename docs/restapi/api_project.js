1db8d64b2f0fb0ebcfe8006fd760878d3146da30
Dave Ross <dave@davidmichaelross.com>
2019-08-28 09:25:34 -0400

    Updated REST API docs

--- docs/restapi/api_project.js
+++ docs/restapi/api_project.js
@@ -9,8 +9,8 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-03-15T13:24:49.215Z",
+    "time": "2019-08-28T13:25:08.533Z",
     "url": "http://apidocjs.com",
-    "version": "0.17.6"
+    "version": "0.17.7"
   }
 });
